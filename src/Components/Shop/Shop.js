73c286033316eb0845b3d7fd04d4a7e568c25a84
PulakBala <pulakbala0524@gmail.com>
2022-10-04 20:47:11 +0600

    ema

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -26,11 +26,19 @@ const Shop = () => {
         }
         setCart(saveCart);
     },[products])
-    const handleAddToCart = (products) =>{
-        // console.log(products);
-        const newCart = [...cart, products]
+    const handleAddToCart = (selectedProduct) =>{
+        let newCart = [];
+        const exists = cart.find(product => product.id === selectedProduct.id);
+        if(!exists){
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct]
+        }else{
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists];
+        }
         setCart(newCart);
-        adToDb(products.id)
+        adToDb(selectedProduct.id)
     }
     return (
         <div className='shop-container'>
