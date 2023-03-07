fbe9a977a7e656f2bf561636666e0133605214d0
Pulak Bala <pulakbala0524@gmail.com>
2023-03-07 17:16:47 +0600

    navber responsive

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,34 +1,43 @@
-import React from 'react';
-import { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../../context/UserContext';
-import logo from '../../images/Logo.svg'
-import './Header.css'
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+import UserContext, { AuthContext } from "../../context/UserContext";
+import logo from "../../images/Logo.svg";
 
 const Header = () => {
-    const {user, logOut} = useContext(AuthContext);
-    return (
-        <nav className='header'>
-            <img src={logo} alt="" />
-            <div>
-                <Link to='/'>Shop</Link>
-                <Link to='/orders'>Order</Link>
-                <Link to='/inventory'>Inventory</Link>
-                <Link to='/about'>About</Link>
-               { 
-                    user?.uid?
-                     <button className='btn-logout' onClick={logOut}>Log Out</button>
-                    :
-
-                    <>
-                        <Link to='/login'>Login</Link>
-                        <Link to='/signup'>Sign up</Link>
-                    </>
-                      
-                }
-            </div>
-        </nav>
-    );
+  const { user, logOut } = UserContext(AuthContext);
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <img src={logo} alt="" />
+        <Navbar.Brand href="#home"></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto"></Nav>
+          <Nav className="gap-4 fs-5 text-center">
+            <Link className="text-white" style={{ textDecoration: "none" }} to="/">Shop</Link>
+            <Link className="text-white" style={{ textDecoration: "none" }} to="/orders">Orders</Link>
+            <Link className="text-white" style={{ textDecoration: "none" }} to="/inventory">Inventory</Link>
+            <Link className="text-white" style={{ textDecoration: "none" }} to="/about">About</Link>
+            {user?.uid ? (
+              <>
+                <button className="btn-logout" onClick={logOut}>
+                  Log Out
+                </button>
+              </>
+            ) : (
+              <>
+                <Link className="text-white" style={{ textDecoration: "none" }} to="/login">Login</Link>
+                <Link className="text-white" style={{ textDecoration: "none" }} to="/signup">SignUp</Link>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
